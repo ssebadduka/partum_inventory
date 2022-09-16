c7d60c2f07364d74e9cd568ee223959137e16fb6
ssebadduka <seba.jamir@gmail.com>
2022-09-16 19:36:27 +0300

    added host

--- partum_inventory/settings.py
+++ partum_inventory/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+#g SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '2j-tes@g+5%*wg!^o8h49br$0^$y7=)ai32vi3&sey2r0j209_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
